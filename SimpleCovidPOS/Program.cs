682c2442b8a1fd9ed7958b78f74f3bfd385c435a
2193079 <55934659+2193079@users.noreply.github.com>
2020-09-30 22:48:38 +0800

    initial commit

---
+++ SimpleCovidPOS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleCovidPOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
